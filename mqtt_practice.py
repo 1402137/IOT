a209ec7cfb54f420ca90809ad456a86e6d7004e6
1402137 <43130235+1402137@users.noreply.github.com>
2019-04-28 14:03:41 +0600

    Delete mqtt_practice.py

--- mqtt_practice.py
+++
@@ -1,7 +0,0 @@
-import paho.mqtt.client as mqtt
-broker_address = "192.168.86.101"
-broker_portno = 1883
-client = mqtt.Client()
-client.connect(broker_address, broker_portno)
-client.subscribe("Test")
-client.publish(topic = "Test", payload = "Hello",qos=0)
